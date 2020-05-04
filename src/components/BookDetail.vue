ac17937bc0eb65d0c500427d13f8030d1746a4f9
Lukasz <lukasz@lukaszcaputa.eu>
2020-05-04 22:41:05 +0200

    refactoring

--- src/components/BookDetail.vue
+++ src/components/BookDetail.vue
@@ -18,129 +18,92 @@
         height="4px"
       ></b-progress>
     </b-alert>
+
     <b-form @submit="onSubmit">
-      <b-form-group id="input-group-1" label="ID" label-for="input-1" label-cols="2">
-        <b-form-input
-          id="input-1"
-          v-model="bookObject.id"
-          required
-        ></b-form-input>
+      <b-form-group  label="ID" label-cols="2">
+        <b-form-input v-model="bookObject.id" readonly></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Title:" label-for="input-2" label-cols="2">
-        <b-form-input
-          id="input-2"
-          v-model="bookObject.title"
-          required
-          placeholder="Enter title"
-        ></b-form-input>
+      <b-form-group label="Title:" label-cols="2">
+        <b-form-input v-model="bookObject.title" required placeholder="Enter title"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Author:" label-for="input-2" label-cols="2">
-        <b-form-input
-          id="input-2"
-          v-model="bookObject.author"
-          required
-          placeholder="Enter Author"
-        ></b-form-input>
+      <b-form-group label="Author:" label-cols="2">
+        <b-form-input v-model="bookObject.author" required placeholder="Enter Author" ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Lead:" label-for="input-2" label-cols="2">
-        <b-form-textarea
-          id="input-2"
-          v-model="bookObject.lead"
-          required
-          placeholder="Enter lead"
-        ></b-form-textarea>
+      <b-form-group label="Lead:"  label-cols="2">
+        <b-form-textarea v-model="bookObject.lead" required placeholder="Enter lead"></b-form-textarea>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Notes:" label-for="input-2" label-cols="2">
-        <b-form-textarea
-          id="input-2"
-          v-model="bookObject.notes"
-          required
-          placeholder="Enter notes"
-        ></b-form-textarea>
+      <b-form-group label="Notes:" label-cols="2">
+        <b-form-textarea v-model="bookObject.notes" required placeholder="Enter notes"></b-form-textarea>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="recommendation:" label-for="input-2" label-cols="2">
-        <b-form-select v-model="bookObject.recommendation" :options="recommendationOptions"></b-form-select>
+      <b-form-group label="Recommendation:" label-cols="2">
+        <b-form-select v-model="bookObject.recommendation" :options="this.getRecommendationOptions"></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="read_date:" label-for="input-2" label-cols="2">
-        <b-form-datepicker id="example-datepicker" v-model="bookObject.read_date" ></b-form-datepicker>
+      <b-form-group label="Read date:" label-cols="2">
+        <b-form-datepicker v-model="bookObject.read_date" ></b-form-datepicker>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="create_tstamp:" label-for="input-2" label-cols="2">
-        <b-form-datepicker id="example-datepicker" v-model="bookObject.create_tstamp" readonly></b-form-datepicker>
+      <b-form-group label="Created:" label-cols="2">
+        <b-form-datepicker v-model="bookObject.create_tstamp" readonly></b-form-datepicker>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="update_tstamp:" label-for="input-2" label-cols="2">
-        <b-form-datepicker id="example-datepicker" v-model="bookObject.update_tstamp" readonly></b-form-datepicker>
+      <b-form-group label="Updated:" label-cols="2">
+        <b-form-datepicker v-model="bookObject.update_tstamp" readonly></b-form-datepicker>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="status:" label-for="input-2" label-cols="2">
-        <b-form-select v-model="bookObject.status" :options="statusOptions"></b-form-select>
+      <b-form-group label="Status:" label-cols="2">
+        <b-form-select v-model="bookObject.status" :options="this.getStatusOptions"></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="image:" label-for="input-2" label-cols="2">
-        <b-form-input
-          id="input-2"
-          v-model="bookObject.image"
-          required
-          placeholder="Enter image"
-        ></b-form-input>
+      <b-form-group  label="Image:" label-cols="2">
+        <b-form-input v-model="bookObject.image" required placeholder="Enter image"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="tags:" label-for="input-2" label-cols="2">
-        <b-form-tags input-id="tags-basic" v-model="bookObject.tags" class="mb-2"></b-form-tags>
+      <b-form-group  label="Tags:"  label-cols="2">
+        <b-form-tags input-id="tags-basic" v-model="currentTags" class="mb-2"></b-form-tags>
       </b-form-group>
-
+      {{ currentTags }}
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ bookObject }}</pre>
-    </b-card>
     <b-row>
-      <b-col><b-button>Save</b-button></b-col>
-      <router-link to="/">
-                        <b-button variant="primary" >Back</b-button>
-                    </router-link>
+      <b-button v-b-toggle.collapse-1 variant="outline-primary">Raw data [json]</b-button>
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <p class="card-text">{{ bookObject }}</p>
+        </b-card>
+      </b-collapse>
     </b-row>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import axios from 'axios'
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'BookDetails',
+  name: 'BookDetail',
   data () {
     return {
       bookObject: {},
-      recommendationOptions: [
-        { value: 1, text: 'Meh' },
-        { value: 2, text: 'Ok' },
-        { value: 3, text: 'Awesome' }
-      ],
-      statusOptions: [
-        { value: 1, text: 'Active' },
-        { value: 9, text: 'Disabled' }
-      ],
+      currentTags: [],
       dismissCountDown: null,
-      showInfoMessage: false
+      showInfoMessage: false,
+      errorMessage: null
     }
   },
   computed: {
-    ...mapState([
-      'books', 'tags'
+    ...mapGetters([
+      'getTagById', 'getBookById', 'getRecommendationOptions', 'getStatusOptions'
     ])
   },
   methods: {
     onSubmit (evt) {
       evt.preventDefault()
-      axios.put('http://localhost:5000/book/update/' + this.bookObject.id, this.bookObject, { crossdomain: true })
+      this.$axios.put('http://localhost:5000/book/update/' + this.bookObject.id, this.bookObject, { crossdomain: true })
         .then(response => {
           this.showInfoMessage = true
         })
@@ -154,27 +117,20 @@ export default {
     }
   },
   mounted () {
-    var bookElem
     const bookIdParam = parseInt(this.$route.params.bookId)
-    for (bookElem in this.books) {
-      if (parseInt(this.books[bookElem].id) === bookIdParam) {
-        this.bookObject = this.books[bookElem]
-        this.bookObject.tags = []
-        axios.get('http://localhost:5000/tag2book/get/' + bookIdParam, { crossdomain: true })
-          .then(response => {
-            for (const tagElem in response.data) {
-              const persistedTagId = response.data[tagElem].tag_id
-
-              for (const tag in this.tags) {
-                if (this.tags[tag].id === persistedTagId) {
-                  this.bookObject.tags.push(this.tags[tag].tag)
-                }
-              }
-            }
-          })
-        break
-      }
-    }
+    this.bookObject = this.getBookById(bookIdParam)
+    this.bookObject.notes = '' // TODO
+    this.$axios.get('http://localhost:5000/tag2book/get/' + bookIdParam, { crossdomain: true })
+      .then(response => {
+        for (const tempTag in response.data) {
+          const tagElem = this.getTagById(response.data[tempTag].tag_id)
+          this.currentTags.push(tagElem.tag)
+          console.log('pushing ' + tagElem.tag)
+        }
+      }).catch(error => {
+        this.errorMessage = error
+        this.dismissCountDown = 5
+      })
   }
 }
 </script>
--- src/store/getters.js
+++ src/store/getters.js
@@ -4,5 +4,24 @@ export default {
   },
   getAllBooks: (state) => {
     return state.books
+  },
+  getTagById: (state) => (id) => {
+    return state.tags.find(tag => tag.id === id)
+  },
+  getBookById: (state) => (id) => {
+    return state.books.find(book => book.id === id)
+  },
+  getRecommendationOptions: (state) => {
+    return [
+      { value: 1, text: 'Meh' },
+      { value: 2, text: 'Ok' },
+      { value: 3, text: 'Awesome' }
+    ]
+  },
+  getStatusOptions: (state) => {
+    return [
+      { value: 1, text: 'Active' },
+      { value: 9, text: 'Disabled' }
+    ]
   }
 }
